0940b93dda7c74d3b9a3aa48a38d5ebd601b11cc
Brian McMichael <brian@brianmcmichael.com>
2020-10-06 13:53:21 -0400

    Add additional libcalls

--- src/DssAction.sol
+++ src/DssAction.sol
@@ -13,18 +13,40 @@ abstract contract DssAction {
         require(ok, "fail");
     }
 
-    // TODO Additional libcall functions for different parameter inputs, and a generic bytes param
+    function libCall(string memory sig) internal {
+        _dcall(abi.encodeWithSignature(sig));
+    }
+
     function libCall(string memory sig, uint256 amount) internal {
         _dcall(abi.encodeWithSignature(sig, amount));
+    }
+
+    function libCall(string memory sig, bytes32 thing) internal {
+        _dcall(abi.encodeWithSignature(sig, thing));
     }
 
     function libCall(string memory sig, bytes32 thing, uint256 amount) internal {
         _dcall(abi.encodeWithSignature(sig, thing, amount));
     }
 
-    // TODO Optional explicit setter for usability
+    function libCall(string memory sig, address addr, uint256 amount) internal {
+        _dcall(abi.encodeWithSignature(sig, addr, amount));
+    }
+
     function setGlobalLine(uint256 amount) internal {
         libCall("setGlobalLine(uint256)", amount);
+    }
+
+    function setIlkLine(bytes32 ilk, uint256 amount) internal {
+        libCall("setIlkLine(bytes32,uint256)", ilk, amount);
+    }
+
+    function setDsr(uint256 rate) internal {
+        libCall("setDSR(uint256)", rate);
+    }
+
+    function setStabilityFee(bytes32 ilk, uint256 rate) internal {
+        libCall("setStabilityFee(bytes32,uint256)", ilk, rate);
     }
 
     // Abstract enforcement of required execute() function
