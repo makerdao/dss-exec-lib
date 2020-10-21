1b859d55fc7099e31dd66334fefb48c7a24e4374
Brian McMichael <brian@brianmcmichael.com>
2020-10-21 10:07:55 -0400

    Add license headers

--- src/DssAction.sol
+++ src/DssAction.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-/// DssAction.sol -- DSS Executive Spell Actions
-
+//
+// DssAction.sol -- DSS Executive Spell Actions
+//
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, Inc.
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
--- src/DssExecLib.sol
+++ src/DssExecLib.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-/// DssExecLib.sol -- MakerDAO Executive Spellcrafting Library
-
+//
+// DssExecLib.sol -- MakerDAO Executive Spellcrafting Library
+//
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, Inc.
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
