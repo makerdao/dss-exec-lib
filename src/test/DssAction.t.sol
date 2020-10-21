2e486491d6892b8c91860c9692a4ff662d59f32e
Brian L. McMichael <brian@brianmcmichael.com>
2020-10-21 10:55:40 -0400

    Add LICENSE and headers (#12)
    * Add LICENSE
    
    * Move testaction to subfolder
    
    * Add license headers to core files
    
    * mv remaining tests to test
    
    * Add license headers
    
    * Uniform pragmas

--- src/test/DssAction.t.sol
+++ src/test/DssAction.t.sol
@@ -1,4 +1,23 @@
-pragma solidity >=0.5.12;
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// DssAction.sol -- DSS Executive Spell Action Tests
+//
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+pragma solidity ^0.6.7;
 
 import "ds-test/test.sol";
 import "ds-token/token.sol";
@@ -28,7 +47,7 @@ import {End}              from 'dss/end.sol';
 import {Spotter}          from 'dss/spot.sol';
 
 import {DssTestAction}    from './DssTestAction.sol';
-import {DssExecLib}       from './DssExecLib.sol';
+import {DssExecLib}       from '../DssExecLib.sol';
 
 interface Hevm {
     function warp(uint256) external;
--- src/test/DssExec.t.sol
+++ src/test/DssExec.t.sol
@@ -1,3 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// DssExec.t.sol -- MakerDAO Executive Spellcrafting Library Tests
+//
+// Copyright (C) 2020 Maker Ecosystem Growth Holdings, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 pragma solidity ^0.6.7;
 
 import "ds-test/test.sol";
@@ -7,9 +26,9 @@ import "ds-value/value.sol";
 
 import "dss-interfaces/Interfaces.sol";
 
-import "./DssExec.sol";
-import "./DssAction.sol";
-import {DssExecLib} from "./DssExecLib.sol";
+import "../DssExec.sol";
+import "../DssAction.sol";
+import {DssExecLib} from "../DssExecLib.sol";
 
 interface Hevm {
     function warp(uint256) external;
