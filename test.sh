179057fc05f2a0cf2e6a823e90c145c88ea25876
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-02-17 18:35:55 -0300

    Some changes for the CI

--- test.sh
+++ test.sh
@@ -3,13 +3,7 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
 
-CI=$1
-
 # shellcheck disable=SC1091
 source "./allow-optimize.sh"
 
-if [[ "$CI" == 1 ]]; then
-    dapp test -v --rpc-url="$ETH_RPC_URL"
-else
-    DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=200 dapp --use solc:0.6.11 test -v --rpc-url="$ETH_RPC_URL"
-fi
+DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=200 dapp --use solc:0.6.11 test -v --rpc-url="$ETH_RPC_URL"
