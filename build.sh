76a12d9ac2c6ffc039b4e60eb040da29c4a871d1
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2021-02-17 15:33:01 -0300

    Fix solc flags

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 # shellcheck disable=SC1091
 source "./allow-optimize.sh"
 
-DAPP_SOLC_OPTIMIZE=true DAPP_BUILD_OPTIMIZE=1 DAPP_SOLC_OPTIMIZE_RUNS=200 dapp --use solc:0.6.11 build
+DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=200 dapp --use solc:0.6.11 build
--- test.sh
+++ test.sh
@@ -11,5 +11,5 @@ source "./allow-optimize.sh"
 if [[ "$CI" == 1 ]]; then
     dapp test -v --rpc-url="$ETH_RPC_URL"
 else
-    DAPP_SOLC_OPTIMIZE=true DAPP_BUILD_OPTIMIZE=1 DAPP_SOLC_OPTIMIZE_RUNS=200 dapp --use solc:0.6.11 test -v --rpc-url="$ETH_RPC_URL"
+    DAPP_BUILD_OPTIMIZE=1 DAPP_BUILD_OPTIMIZE_RUNS=200 dapp --use solc:0.6.11 test -v --rpc-url="$ETH_RPC_URL"
 fi
