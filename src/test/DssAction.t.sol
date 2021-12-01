005429677088e6492457ba8ed216fd39d9854e73
Brian McMichael <brian@brianmcmichael.com>
2021-12-01 10:37:23 -0500

    mock function visibility

--- src/test/DssAction.t.sol
+++ src/test/DssAction.t.sol
@@ -80,14 +80,14 @@ contract UniPairMock {
 
 contract AaveMock {
     // https://docs.aave.com/developers/the-core-protocol/lendingpool
-    function getReserveData(address dai) public returns (
+    function getReserveData(address dai) public view returns (
         uint256, uint128, uint128, uint128, uint128, uint128, uint40, address, address, address, address, uint8
     ) {
         address _dai = dai; // avoid stack too deep
         return (0,0,0,0,0,0,0, _dai, _dai, _dai, address(this), 0);
     }
 
-    function getMaxVariableBorrowRate() public returns (uint256) {
+    function getMaxVariableBorrowRate() public pure returns (uint256) {
         return type(uint256).max;
     }
 }
