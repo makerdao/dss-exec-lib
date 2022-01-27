6d68f5c861d753039ac5954766ce27d4db29968a
Kaue Cano <30631118+canokaue@users.noreply.github.com>
2022-01-27 09:36:16 -0300

    Update src/CollateralOpts.sol copyright
    Co-authored-by: Chris Smith <1408372+iamchrissmith@users.noreply.github.com>

--- src/CollateralOpts.sol
+++ src/CollateralOpts.sol
@@ -2,7 +2,7 @@
 //
 // CollateralOpts.sol -- DSS Executive Spell Collateral Options
 //
-// Copyright (C) 2021 Maker Ecosystem Growth Holdings, Inc.
+// Copyright (C) 2021-2022 Dai Foundation
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
