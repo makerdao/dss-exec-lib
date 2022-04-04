06d5b75b2262bf473eb1fceb2804bb4ad32987bd
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-04-04 13:52:20 -0400

    chore: add rpow to DssAction.t.sol

--- src/test/DssAction.t.sol
+++ src/test/DssAction.t.sol
@@ -163,6 +163,32 @@ contract ActionTest is DSTest {
         require(y == 0 || z / y == x);
         z = z / RAY;
     }
+    function rpow(uint256 x, uint256 n, uint256 b) internal pure returns (uint256 z) {
+        assembly {
+            switch n case 0 { z := b }
+            default {
+                switch x case 0 { z := 0 }
+                default {
+                    switch mod(n, 2) case 0 { z := b } default { z := x }
+                    let half := div(b, 2)  // for rounding.
+                    for { n := div(n, 2) } n { n := div(n,2) } {
+                        let xx := mul(x, x)
+                        if shr(128, x) { revert(0,0) }
+                        let xxRound := add(xx, half)
+                        if lt(xxRound, xx) { revert(0,0) }
+                        x := div(xxRound, b)
+                        if mod(n,2) {
+                            let zx := mul(z, x)
+                            if and(iszero(iszero(x)), iszero(eq(div(zx, x), z))) { revert(0,0) }
+                            let zxRound := add(zx, half)
+                            if lt(zxRound, zx) { revert(0,0) }
+                            z := div(zxRound, b)
+                        }
+                    }
+                }
+            }
+        }
+    }
     function min(uint x, uint y) internal pure returns (uint z) {
         (x >= y) ? z = y : z = x;
     }
